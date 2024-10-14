56a33b43d59a4df66900f4bcd58628b62d968a9d
Vladimir Sinkovets <vovasin49@gmail.com>
2024-10-14 19:28:27 +0300

    add password confirmation

--- Voting_App/ViewModels/RegisterViewModel.cs
+++ Voting_App/ViewModels/RegisterViewModel.cs
@@ -6,7 +6,12 @@ namespace Voting_App.ViewModels
     {
         [Required]
         public string Email { get; set; }
+        
         [Required]
         public string Password { get; set; }
+        
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
     }
 }
