9fbb8952f607439deac195ad1c4b4ed7b785bb2c
Vladimir Sinkovets <vovasin49@gmail.com>
2024-10-14 20:26:09 +0300

    add email validation

--- Voting_App/ViewModels/RegisterViewModel.cs
+++ Voting_App/ViewModels/RegisterViewModel.cs
@@ -5,6 +5,7 @@ namespace Voting_App.ViewModels
     public class RegisterViewModel
     {
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
         
         [Required]
